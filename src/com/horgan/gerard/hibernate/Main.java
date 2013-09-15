fca9bc31fc46627321a71d0f0b8acbfacfec9324
Ger123 <gerard.horgan1@gmail.com>
2013-09-15 11:15:09 +0100

    hibernate...creating database

---
+++ src/com/horgan/gerard/hibernate/Main.java
@@ -0,0 +1,27 @@
+package com.horgan.gerard.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Student_Info student = new Student_Info();
+		
+		student.setName("Hoggy");
+		student.setRollNo(1);
+		
+		SessionFactory sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		
+		session.save(student);
+		
+		session.getTransaction().commit();
+		session.close();
+		sessionFactory.close();
+	}
+
+}
